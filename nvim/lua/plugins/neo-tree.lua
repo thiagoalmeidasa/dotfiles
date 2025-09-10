eff4e2469fa1352fbcd785919626f538e0a0540a
Thiago Almeida <thiagoalmeidasa@gmail.com>
2025-09-10 14:22:31 +0200

    feat(nvim): add neo-tree configuration and related plugins
    - Set neo-tree window auto_expand_width option to true.
    - Included nvim-lsp-file-operations plugin for enhanced file operations in LSP.
    - Added nvim-window-picker plugin with specific filter rules to improve window management.

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -2,147 +2,179 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
-  },
-  opts = {
-    close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
-    default_component_configs = {
-      icon = {
-        folder_empty = '',
-        folder_empty_open = '',
-        default = '',
-      },
-      modified = {
-        symbol = '•',
-      },
-      name = {
-        trailing_slash = true,
-        highlight_opened_files = true, -- NeoTreeFileNameOpened
-        use_git_status_colors = true,
-      },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
     },
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-          ['<S-CR>'] = 'system_open',
-          O = 'system_open',
-          Y = 'copy_selector',
-          h = 'parent_or_close',
-          l = 'child_or_open',
-          ['oa'] = 'avante_add_files',
+    cmd = 'Neotree',
+    keys = {
+      { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
+    },
+    opts = {
+      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+      default_component_configs = {
+        icon = {
+          folder_empty = '',
+          folder_empty_open = '',
+          default = '',
+        },
+        modified = {
+          symbol = '•',
+        },
+        name = {
+          trailing_slash = true,
+          highlight_opened_files = true, -- NeoTreeFileNameOpened
+          use_git_status_colors = true,
         },
       },
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-        hide_by_name = {
-          '.git',
-          '.DS_Store',
-          'thumbs.db',
+      filesystem = {
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+            ['<S-CR>'] = 'system_open',
+            O = 'system_open',
+            Y = 'copy_selector',
+            h = 'parent_or_close',
+            l = 'child_or_open',
+            ['oa'] = 'avante_add_files',
+          },
+          auto_expand_width = true,
         },
-        never_show = {},
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            '.git',
+            '.DS_Store',
+            'thumbs.db',
+          },
+          never_show = {},
+        },
       },
-    },
 
-    commands = {
-      system_open = function(state)
-        -- TODO: remove deprecated method check after dropping support for neovim v0.9
-        vim.ui.open(state.tree:get_node():get_id())
-      end,
-      parent_or_close = function(state)
-        local node = state.tree:get_node()
-        if node:has_children() and node:is_expanded() then
-          state.commands.toggle_node(state)
-        else
-          require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
-        end
-      end,
-      child_or_open = function(state)
-        local node = state.tree:get_node()
-        if node:has_children() then
-          if not node:is_expanded() then -- if unexpanded, expand
+      commands = {
+        system_open = function(state)
+          -- TODO: remove deprecated method check after dropping support for neovim v0.9
+          vim.ui.open(state.tree:get_node():get_id())
+        end,
+        parent_or_close = function(state)
+          local node = state.tree:get_node()
+          if node:has_children() and node:is_expanded() then
             state.commands.toggle_node(state)
-          else -- if expanded and has children, seleect the next child
-            if node.type == 'file' then
-              state.commands.open(state)
-            else
-              require('neo-tree.ui.renderer').focus_node(state, node:get_child_ids()[1])
+          else
+            require('neo-tree.ui.renderer').focus_node(state, node:get_parent_id())
+          end
+        end,
+        child_or_open = function(state)
+          local node = state.tree:get_node()
+          if node:has_children() then
+            if not node:is_expanded() then -- if unexpanded, expand
+              state.commands.toggle_node(state)
+            else -- if expanded and has children, seleect the next child
+              if node.type == 'file' then
+                state.commands.open(state)
+              else
+                require('neo-tree.ui.renderer').focus_node(state, node:get_child_ids()[1])
+              end
             end
+          else -- if has no children
+            state.commands.open(state)
           end
-        else -- if has no children
-          state.commands.open(state)
-        end
-      end,
-      copy_selector = function(state)
-        local node = state.tree:get_node()
-        local filepath = node:get_id()
-        local filename = node.name
-        local modify = vim.fn.fnamemodify
+        end,
+        copy_selector = function(state)
+          local node = state.tree:get_node()
+          local filepath = node:get_id()
+          local filename = node.name
+          local modify = vim.fn.fnamemodify
 
-        local vals = {
-          ['BASENAME'] = modify(filename, ':r'),
-          ['EXTENSION'] = modify(filename, ':e'),
-          ['FILENAME'] = filename,
-          ['PATH (CWD)'] = modify(filepath, ':.'),
-          ['PATH (HOME)'] = modify(filepath, ':~'),
-          ['PATH'] = filepath,
-          ['URI'] = vim.uri_from_fname(filepath),
-        }
+          local vals = {
+            ['BASENAME'] = modify(filename, ':r'),
+            ['EXTENSION'] = modify(filename, ':e'),
+            ['FILENAME'] = filename,
+            ['PATH (CWD)'] = modify(filepath, ':.'),
+            ['PATH (HOME)'] = modify(filepath, ':~'),
+            ['PATH'] = filepath,
+            ['URI'] = vim.uri_from_fname(filepath),
+          }
 
-        local options = vim.tbl_filter(function(val)
-          return vals[val] ~= ''
-        end, vim.tbl_keys(vals))
-        if vim.tbl_isempty(options) then
-          vim.notify('No values to copy', vim.log.levels.WARN)
-          return
-        end
-        table.sort(options)
-        vim.ui.select(options, {
-          prompt = 'Choose to copy to clipboard:',
-          format_item = function(item)
-            return ('%s: %s'):format(item, vals[item])
-          end,
-        }, function(choice)
-          local result = vals[choice]
-          if result then
-            vim.notify(('Copied: `%s`'):format(result))
-            vim.fn.setreg('+', result)
+          local options = vim.tbl_filter(function(val)
+            return vals[val] ~= ''
+          end, vim.tbl_keys(vals))
+          if vim.tbl_isempty(options) then
+            vim.notify('No values to copy', vim.log.levels.WARN)
+            return
           end
-        end)
-      end,
-      avante_add_files = function(state)
-        local node = state.tree:get_node()
-        local filepath = node:get_id()
-        local relative_path = require('avante.utils').relative_path(filepath)
+          table.sort(options)
+          vim.ui.select(options, {
+            prompt = 'Choose to copy to clipboard:',
+            format_item = function(item)
+              return ('%s: %s'):format(item, vals[item])
+            end,
+          }, function(choice)
+            local result = vals[choice]
+            if result then
+              vim.notify(('Copied: `%s`'):format(result))
+              vim.fn.setreg('+', result)
+            end
+          end)
+        end,
+        avante_add_files = function(state)
+          local node = state.tree:get_node()
+          local filepath = node:get_id()
+          local relative_path = require('avante.utils').relative_path(filepath)
 
-        local sidebar = require('avante').get()
+          local sidebar = require('avante').get()
 
-        local open = sidebar:is_open()
-        -- ensure avante sidebar is open
-        if not open then
-          require('avante.api').ask()
-          sidebar = require('avante').get()
-        end
+          local open = sidebar:is_open()
+          -- ensure avante sidebar is open
+          if not open then
+            require('avante.api').ask()
+            sidebar = require('avante').get()
+          end
 
-        sidebar.file_selector:add_selected_file(relative_path)
+          sidebar.file_selector:add_selected_file(relative_path)
 
-        -- remove neo tree buffer
-        if not open then
-          sidebar.file_selector:remove_selected_file 'neo-tree filesystem [1]'
-        end
-      end,
+          -- remove neo tree buffer
+          if not open then
+            sidebar.file_selector:remove_selected_file 'neo-tree filesystem [1]'
+          end
+        end,
+      },
     },
+  },
+  {
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neo-tree/neo-tree.nvim', -- makes sure that this loads after Neo-tree.
+    },
+    config = function()
+      require('lsp-file-operations').setup()
+    end,
+  },
+  {
+    's1n7ax/nvim-window-picker',
+    version = '2.*',
+    config = function()
+      require('window-picker').setup {
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          -- filter using buffer options
+          bo = {
+            -- if the file type is one of following, the window will be ignored
+            filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+            -- if the buffer type is one of following, the window will be ignored
+            buftype = { 'terminal', 'quickfix' },
+          },
+        },
+      }
+    end,
   },
 }
